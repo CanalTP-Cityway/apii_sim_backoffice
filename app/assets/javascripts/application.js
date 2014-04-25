b9e44a4fd0103050532f345352b100c307281bec
Luc Donnet <ldonnet@cityway.fr>
2014-04-25 14:13:31 +0200

    Update view rendering

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require OpenLayers
+//= require map_layers
 //= require_tree .
--- app/controllers/mi_systems_controller.rb
+++ app/controllers/mi_systems_controller.rb
@@ -1,7 +1,19 @@
 class MiSystemsController < InheritedResources::Base
 
+  def show
+    @map = map
+    show!
+  end 
+  
 	private
 	def permitted_params
 		params.permit(:widget => [:name, :comment, :api_url, :api_key, :start_date, :end_date, :multiple_starts_and_arrivals])
 	end
+
+  def map
+    MapLayers::JsExtension::MapBuilder.new("map") do |builder, page|
+      page << builder.map.add_layer(MapLayers::OpenLayers::OSM_MAPNIK)
+      page << builder.map.zoom_to_max_extent()
+    end
+  end
 end
