3a48199f096b83907c57b8656b787c10cadc3641
Marc Florisson <mflorisson@gmail.com>
2014-04-25 09:59:40 +0200

    rename type column for stop, language keyword

--- db/migrate/20140422114138_create_stops.rb
+++ db/migrate/20140422114138_create_stops.rb
@@ -4,7 +4,7 @@ class CreateStops < ActiveRecord::Migration
       t.integer :stop_code
       t.references :mi_system, index: true
       t.string :name, limit: 255
-      t.integer :type
+      t.integer :stop_type
       t.text :administrative_code, limit: 255
       t.integer :site_ref
       t.text :transport_mode, limit: 255
