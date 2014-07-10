520bfa6dda4b36bfc18a38097e5000f585efa069
Zakaria BOUZIANE <zbouziane@gmail.com>
2014-07-10 15:00:48 +0200

    Validating presence of start and end stops for connections

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,4 +1,6 @@
 class Connection < ActiveRecord::Base 
   belongs_to :stop_1, :class_name => "Stop"
+  validates :stop_1, presence: true
   belongs_to :stop_2, :class_name => "Stop"
+  validates :stop_2, presence: true
 end
